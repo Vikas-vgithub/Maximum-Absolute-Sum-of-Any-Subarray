b8e1b9b7b92e4451ea8159866876de49a1475820
Vikas Verma <110339905+Vikas-vgithub@users.noreply.github.com>
2024-03-15 21:11:50 +0530

    Create Maximum Absolute Sum of Any Subarray.java

---
+++ Maximum Absolute Sum of Any Subarray.java
@@ -0,0 +1,32 @@
+class Solution {
+   public int maxAbsoluteSum(int[] nums) {
+    int sum=0,max=Integer.MIN_VALUE;
+    for(int i=0;i<nums.length;i++)
+    {
+        sum+=nums[i];
+        if(sum>max)
+        {
+            max=sum;
+        }
+        if(sum<0)
+        {
+            sum=0;
+        }
+    }
+    sum=0;
+    int min=Integer.MAX_VALUE;
+    for(int i=0;i<nums.length;i++)
+    {
+        sum+=nums[i];
+        if(sum<min)
+        {
+            min=sum;
+        }
+        if(sum>0)
+        {
+            sum=0;
+        }
+    }
+    return Math.max(Math.abs(max),Math.abs(min));
+}
+}
